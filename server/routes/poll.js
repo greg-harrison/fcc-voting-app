fb72f7c0c8fed08e3d6d0ff65a865e64a82296e8
Greg <greg@Gregs-MacBook-Pro.local>
2017-09-14 16:20:10 -0400

    Lets get this stuff hooked up

--- server/routes/poll.js
+++ server/routes/poll.js
@@ -1,5 +1,5 @@
 const express = require('express')
-const router = express.router()
+const router = express.Router()
 const pollController = require('../controllers/poll')
 
 // assume starts with /poll
--- server/server.js
+++ server/server.js
@@ -3,6 +3,7 @@ const routes = require('./routes')
 const cors = require('cors')
 const middleware = require('./middleware')
 const port = process.env.apiPort || 8081
+const dotenv = require('dotenv').config()
 
 const app = express()
 
@@ -12,5 +13,6 @@ app.use(middleware.malformedUrl)
 app.use('/', routes)
 
 app.listen(port, () => {
+  console.log('process.env.test', process.env.test);
   console.log(`Server is running on port ${port}`);
 })
