744c29953eb6d35c0684305230f5bbb0a7a4e35b
Greg <greg@Gregs-MacBook-Pro.local>
2017-09-21 15:46:57 -0400

    updated some things

---
+++ server/controllers/auth.js
@@ -0,0 +1,11 @@
+const mainController = require('./main')
+const auth0 = require('auth0-js')
+
+module.exports = {
+  handleLogin: (req, res) => {
+
+    console.log('req', req);
+    console.log('res', res);
+    console.log('json', json);
+  }
+}
