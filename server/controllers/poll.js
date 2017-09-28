c1e1d1e8e50a750898686b1723f8c8d1163e2cf6
Greg <harrison.greg.c@gmail.com>
2017-09-27 20:53:33 -0400

    updated several api calls

--- server/controllers/poll.js
+++ server/controllers/poll.js
@@ -1,42 +1,50 @@
 const mainController = require('./main')
 const DB = require('../models/')
 
-module.exports = {
-  getPoll: (req, res) => {
-    console.log('req', req);
-    console.log('res', res);
-  },
-  getPollResponses: (req, res) => {
-    console.log('req', req);
-    console.log('res', res);
+exports.getPoll = (req, res) => {
+  console.log('req', req);
+  console.log('res', res);
 
-  },
-  getUserCreatedPolls: (req, res) => {
-    //   DB.poll.findAll({
-    //     where: ({
-    //       user_id_created: req.params.user_id
-    //     }),
-    //     include: [
-    //       {
-    //         model: DB.user
-    //       }
-    //     ]
-    //   })
-    //     .then((poll) => {
-    //       console.log('res', res);
-    //       res.status(200).send(poll);
-    //     })
-  },
-  createPoll: (req, res) => {
-    console.log('req', req);
-    console.log('res', res);
-  },
-  editPoll: (req, res) => {
-    console.log('req', req);
-    console.log('res', res);
-  },
-  respondToPoll: (req, res) => {
-    console.log('req', req);
-    console.log('res', res);
-  }
+  res.status(200)
+}
+exports.getPollResponses = (req, res) => {
+  console.log('req', req);
+  console.log('res', res);
+
+  res.status(200)
+}
+exports.getUserCreatedPolls = (req, res) => {
+  //   DB.poll.findAll({
+  //     where: ({
+  //       user_id_created: req.params.user_id
+  //     }),
+  //     include: [
+  //       {
+  //         model: DB.user
+  //       }
+  //     ]
+  //   })
+  //     .then((poll) => {
+  //       console.log('res', res);
+  //       res.status(200).send(poll);
+  //     })
+  res.status(200)
+}
+exports.createPoll = (req, res) => {
+  console.log('req', req);
+  console.log('res', res);
+
+  res.status(200)
+}
+exports.editPoll = (req, res) => {
+  console.log('req', req);
+  console.log('res', res);
+
+  res.status(200)
+}
+exports.respondToPoll = (req, res) => {
+  console.log('req', req);
+  console.log('res', res);
+
+  res.status(200)
 }
