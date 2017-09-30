46134419982ad05428c979ecf160f80b49f58893
Greg <harrison.greg.c@gmail.com>
2017-09-30 11:06:33 -0400

    user queries are set up. yay

--- server/queries/user.js
+++ server/queries/user.js
@@ -1,19 +1,24 @@
 const db = require('../db')
 const pgp = db.$config.pgp;
 
-exports.createUser = (req, res) => {
-  let data = {
-    user_id: 2,
-    name: 'Greg Test',
-    email: 'harrison.test@gmail.com'
-  }
-  db.user.create(data).then((user) => {
-    console.log(user.get())
-  })
-  res.status(200)
+exports.createUser = (req, res, next) => {
+  // db.none checks to see that there doesn't already exist a record matching the criteria of the query
+  db.none('insert into public.user(name,email,password)' +
+    'values(${name},${email},${password})',
+    req.body)
+    .then(function () {
+      res.status(200)
+        .json({
+          status: 'success',
+          message: 'Inserted a user'
+        })
+    })
+    .catch(function (err) {
+      return next(err)
+    })
 }
+
 exports.getUser = (req, res, next) => {
-  console.log('req', req.params);
   var userId = parseInt(req.params.user_id);
   db.one('select * from public.user where user_id = $1', userId)
     .then(function (data) {
@@ -21,22 +26,25 @@ exports.getUser = (req, res, next) => {
         .json({
           status: 'success',
           data: data,
-          message: 'Retrieved ONE human'
+          message: 'Retrieved one user'
         });
     })
     .catch(function (err) {
       return next(err);
     });
-
-  // db.user.findAll({
-  //   where: ({
-  //     user_id: req.params.user_id
-  //   })
-  // }).then((user) => {
-  //   console.log('user', user);
-  //   console.log('res', res);
-  //   res.status(200).send(user);
-  // })
-  // console.log('res', res);
 }
 
+exports.getAllUsers = (req, res, next) => {
+  db.manyOrNone('select * from public.user')
+    .then(function (data) {
+      res.status(200)
+        .json({
+          status: 'success',
+          data: data,
+          message: 'Retrieved all users'
+        });
+    })
+    .catch(function (err) {
+      return next(err);
+    });
+}
