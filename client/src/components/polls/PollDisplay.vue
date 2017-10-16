9edf0f11a7a9193ab9a326b2354bb4692d7be712
Greg <harrison.greg.c@gmail.com>
2017-10-15 22:04:53 -0400

    Pulling a poll from the DB. Did a Where on an inner join. very excited

---
+++ client/src/components/polls/PollDisplay.vue
@@ -0,0 +1,65 @@
+<template>
+  <div class="poll display container p-0">
+    <div class="card">
+      <div class="card-header">
+        Hello Poll
+      </div>
+      <div class="card-body">
+        {{ poll }}
+        <p v-if="error" class="error">
+          {{ error }}, please try again
+        </p>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import poll from '../../restCalls/polls'
+
+export default {
+  data: () => ({
+    poll: {}
+  }),
+  methods: {
+    getPoll() {
+      let credentials = {
+        poll_id: 1
+      }
+
+      poll.getPoll(this, credentials)
+    },
+  },
+
+  beforeCreate() {
+    console.log('beforeCreate')
+  },
+  created() {
+    console.log('created')
+  },
+  beforeMount() {
+    this.getPoll()
+    console.log('beforeMount')
+  },
+  mounted() {
+    console.log('mounted')
+  },
+  beforeUpdate() {
+    console.log('beforeUpdate')
+  },
+  updated() {
+    console.log('updated')
+  },
+  beforeDestroy() {
+    console.log('beforeDestroy')
+  },
+  destroyed() {
+    console.log('destroyed')
+  }
+
+}
+</script>
+
+<style lang="sass" scoped>
+
+</style>
