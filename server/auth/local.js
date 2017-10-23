776f795b4a4b221f7e6823d3b4c710c09bbf7c35
Greg <harrison.greg.c@gmail.com>
2017-10-22 23:24:10 -0400

    Update. Passport session and writing to Express Obj is still not working..

--- server/auth/local.js
+++ server/auth/local.js
@@ -9,8 +9,7 @@ const options = {
 
 module.exports = new LocalStrat(options, (username, password, done) => {
   //Check the DB for the user's email -- This is what we'll use in place of a username
-  console.log('HELLO WORLD');
-  console.log('username', username);
+
 
   // db.one will reject if there are more than on entry, making it perfect for this
   db.one('select * from public.user where email = $1', username)
