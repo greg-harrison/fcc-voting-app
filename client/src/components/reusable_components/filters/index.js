d132e0e633bddf08339742b5d535d0a63f3b8b95
Greg <greg@Gregs-MacBook-Pro.local>
2017-11-14 08:49:55 -0500

    Added several String filters and built tests to verify them

--- client/src/components/reusable_components/filters/index.js
+++ client/src/components/reusable_components/filters/index.js
@@ -1,10 +1,35 @@
 module.exports = {
+  // Strings
   capitalize: function (value) {
-    if (!value) return "";
+    // 'hello' => 'Hello'
+    if (!value || value !== 0) return "";
     value = value.toString();
     return value.charAt(0).toUpperCase() + value.slice(1);
   },
+  uppercase: function (value) {
+    // 'hello' => 'HELLO'
+    return (value || value === 0)
+      ? value.toString().toUpperCase()
+      : ''
+  },
+  lowercase: function (value) {
+    // 'HELLO' => 'hello'
+    return (value || value === 0)
+      ? value.toString().toLowerCase()
+      : ''
+  },
+  truncate: function (value, length) {
+    // 'VeryLongWordThatIsLong'     => 'VeryLongWordTha...'
+    // 'VeryLongWordThatIsLong', 18 => 'VeryLongWordThatIs...'
+    length = length || 15
+    if (!value || typeof value !== 'string') return ''
+    if (value.length <= length) return value
+    return value.substring(0, length) + '...'
+  },
+
+  // Ints
   roundToDecimals: function (value) {
+    // 20.165 => 20.17
     if (!value) return "";
     return Math.round(value * 100) / 100;
   }
--- client/src/main.js
+++ client/src/main.js
@@ -4,6 +4,7 @@ import Vue from 'vue'
 import App from './App'
 import router from './router'
 import auth from './auth'
+import 'babel-polyfill'
 
 // Vue.use(BootstrapVue)
 
