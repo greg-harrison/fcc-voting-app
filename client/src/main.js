4ffa2e6f38459d3b34baf449eaba8cb11b0725f1
Greg <harrison.greg.c@gmail.com>
2017-11-23 11:18:49 -0500

    Vuex started and showing up in the AMAZING Vue Dev Tools

--- client/src/main.js
+++ client/src/main.js
@@ -1,6 +1,7 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
+import Vuex from 'vuex'
 import App from './App'
 import router from './router'
 import auth from './auth'
@@ -10,10 +11,17 @@ import 'babel-polyfill'
 
 Vue.config.productionTip = false
 
+const store = new Vuex.Store({
+  state: {
+    count: 0
+  }
+})
+
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
   router,
   template: '<App/>',
-  components: { App }
+  components: { App },
+  store: store
 })
